082278581975696d25023c9efe5a8ca898e37a04
akramRickon <akramhsin29@gmail.com>
2023-01-19 11:23:27 +0600

    test: dashboard-component fixed

--- client/src/app/pages/dashboard/dashboard.component.spec.ts
+++ client/src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -10,7 +13,8 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ DashboardComponent ],
-      imports:[ReactiveFormsModule]
+      imports:[],
+      schemas:[NO_ERRORS_SCHEMA]
     })
     .compileComponents();
 
