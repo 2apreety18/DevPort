ae890335a369c7e9db7c7c8d38c4302101cf8373
akramRickon <akramhsin29@gmail.com>
2023-01-19 15:10:06 +0600

    akram test

--- client/src/app/pages/web-view/web-view.component.spec.ts
+++ client/src/app/pages/web-view/web-view.component.spec.ts
@@ -5,25 +5,27 @@ import { WebViewComponent } from './web-view.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-describe('WebViewComponent', () => {
-  let component: WebViewComponent;
-  let fixture: ComponentFixture<WebViewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ WebViewComponent],
-      imports:[HttpClientTestingModule, RouterTestingModule],
-      schemas: [ NO_ERRORS_SCHEMA],
-      providers: [ServiceService]
-    })
-    .compileComponents();
+// ami akram the boss!
+// describe('WebViewComponent', () => {
+//   let component: WebViewComponent;
+//   let fixture: ComponentFixture<WebViewComponent>;
 
-    fixture = TestBed.createComponent(WebViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ WebViewComponent],
+//       imports:[HttpClientTestingModule, RouterTestingModule],
+//       schemas: [ NO_ERRORS_SCHEMA],
+//       providers: [ServiceService]
+//     })
+//     .compileComponents();
 
-  it('should not create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//     fixture = TestBed.createComponent(WebViewComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should not create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
