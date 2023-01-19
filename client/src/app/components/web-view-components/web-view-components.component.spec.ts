0e392312ecf94a1675525cb05d02cacf36a17898
akramRickon <akramhsin29@gmail.com>
2023-01-19 14:51:13 +0600

    'merge conflit at web page solved"

--- client/src/app/components/web-view-components/web-view-components.component.spec.ts
+++ client/src/app/components/web-view-components/web-view-components.component.spec.ts
@@ -1,9 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BasicInfoWebComponent } from './basic-info-web/basic-info-web.component';
-import { ContactFormWebComponent } from './contact-form-web/contact-form-web.component';
-import { EducationsWebComponent } from './educations-web/educations-web.component';
-import { ExperiencesWebComponent } from './experiences-web/experiences-web.component';
-import { ProjectsWebComponent } from './projects-web/projects-web.component';
+
 
 import { WebViewComponentsComponent } from './web-view-components.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -13,14 +9,23 @@ import { ServiceService } from 'src/app/services/service.service';
 import { profileData } from 'src/app/mocks/profile';
 
 describe('WebViewComponentsComponent', () => {
-  TestBed.configureTestingModule({
-    declarations: [WebViewComponentsComponent],
-    schemas: [NO_ERRORS_SCHEMA]
-  })
-    .compileComponents();
   let component: WebViewComponentsComponent;
   let fixture: ComponentFixture<WebViewComponentsComponent>;
 
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WebViewComponentsComponent],
+      imports:[HttpClientTestingModule, RouterTestingModule],
+      schemas: [ NO_ERRORS_SCHEMA],
+      providers: [ServiceService, profileData]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WebViewComponentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
 
   it('should create', () => {
