a59ce236961d5b5985595788a2c2b09fcb9134a8
akramRickon <akramhsin29@gmail.com>
2023-01-21 13:54:12 +0600

    signup form complete

--- client/src/app/pages/signin/signup/signup/signup.component.spec.ts
+++ client/src/app/pages/signin/signup/signup/signup.component.spec.ts
@@ -6,7 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { SignupComponent } from './signup.component';
 import { ServiceService } from 'src/app/services/service.service';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -22,9 +22,11 @@ describe('SignupComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
   it('should check email validation while signup', () => {
     let email = component.signInForm.controls['email'];
     email.setValue('test@gmail.com');
@@ -40,11 +42,12 @@ describe('SignupComponent', () => {
     expect(password.value).toEqual('12345678');
     expect(password.invalid).toBeFalsy();
   })
+
   it('should check invalid when inputs are empty - Reactive form', () => {
     expect(component.signInForm.invalid).toBeTruthy();
   })
 
-  it('should disableattribute on submit button when signup form is invalid', () => {
+  it('should disable attribute on submit button when signup form is invalid', () => {
     component.signInForm.get('email')?.setValue(null);
     const signupBtn = fixture.debugElement.query(By.css('button')).nativeElement;
     expect(signupBtn.disabled).toBeTrue();
@@ -56,4 +59,27 @@ describe('SignupComponent', () => {
     fixture.debugElement.query(By.css('button')).triggerEventHandler('click', null);
     expect(component.handleSubmit).toHaveBeenCalled();
   })
-});
+
+  it('Should check form control inside a form group', fakeAsync(()=>{
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const email: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email');
+      const password: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
+
+      email.value = 'shuhat@gmail.com';
+      password.value = '123456';
+
+      email.dispatchEvent(new Event('input'));
+      password.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      fixture.whenStable().then(() => {
+        expect(component.signInForm.value).toEqual({
+          email: 'shuhat@gmail.com',
+          password: '123456'
+        })
+      })
+    })
+  }));
+
+})
